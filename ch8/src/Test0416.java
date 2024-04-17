7368129a8f4e2da95d7112ef1ef128ba67fe715e
DJL1n <djl1n_private@163.com>
2024-04-17 15:02:55 +0800

    2024.4.17

--- ch8/src/Test0416.java
+++ ch8/src/Test0416.java
@@ -16,6 +16,25 @@ public class Test0416 {
         int indexOne=path.indexOf("\\");
         int indexTwo=path.lastIndexOf("\\");
 
-        //substring
+        //substring 实质上就是切片
+        String str=tom.substring(1,5);
+        System.out.println(str);
+
+        //trim 去除两边空格，得到的是一个新String
+        String jerry=" iuwfheiufhew ";
+        System.out.println(jerry.trim());
+
+        //将String转化为其他数据类型
+        int x;
+        String s="876";
+        x=Integer.parseInt(s);
+
+        //将其他数据类型转化为String
+        String str1=String.valueOf(x);
+        System.out.println(str1);
+
+        //进制表示
+        String str2=Integer.toBinaryString(x);
+        System.out.println(str2);
     }
 }
