83ace2c19bbd9658b6c6f2c0d191c5f93792c2ee
DJL1n <djl1n_private@163.com>
2024-04-17 10:25:35 +0800

    2024.4.17

---
+++ test/Try0417.java
@@ -0,0 +1,26 @@
+class A{
+    int t=2;
+    double f(double m, double n){//m=4,n=5
+        t=t*t;//4 //16
+        return n+m+t;//13  //25
+    }
+}
+
+class B extends A{
+    int t=3;
+    double f(double m,double n){//m=4,n=5
+        double c=super.f(m,n);//13
+        return m*n+t*super.t;//32 //68
+    }
+}
+
+
+
+public class Try0417 {
+    public static void main(String[] args) {
+       double m=4,n=5;
+       B b=new B();
+        System.out.println(b.f(m, n));
+        System.out.println(b.f(m, n));
+    }
+}
