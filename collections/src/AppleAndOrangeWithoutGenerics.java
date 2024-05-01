6f591acceb1b1874a6317c30aa7515bbf439f35f
DJL1n <djl1n_private@163.com>
2024-05-01 09:37:39 +0800

    2024.5.1

--- collections/src/AppleAndOrangeWithoutGenerics.java
+++ collections/src/AppleAndOrangeWithoutGenerics.java
@@ -25,12 +25,13 @@ class Orange{
 //上面的都是Object的子类所以可以放进去，但是不能强转
 //下面使用泛型的概念
 public class AppleAndOrangeWithoutGenerics {
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        ArrayList apples=new ArrayList();
+        ArrayList<Apple> apples=new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             apples.add(new Apple());
         }
+        //编译时错误
+        //泛型规定可以向里面保存的对象类型，尖括号里面可以写多个，等号右边不需要写
         apples.add(new Orange());
         for (Object apple : apples) {
             ((Apple)apple).id();
