bba1784180e445ac9e4de738c9ab255e2c50a873
DJL1n <djl1n_private@163.com>
2024-05-11 19:17:47 +0800

    2024.5.11

--- collections/src/LinkedListFeatures.java
+++ collections/src/LinkedListFeatures.java
@@ -7,5 +7,10 @@ public class LinkedListFeatures {
         System.out.println(pets);
         //完全相同
         System.out.println("pets.getFirst():" + pets.getFirst());
+        System.out.println("pets.element():" + pets.element());
+        //仅当列表为空时存在区别
+        System.out.println("pets.peek() = " + pets.peek());
+        //完全相同；移除并返回第一个元素
+        System.out.println("pets.remove() = " + pets.remove());
     }
 }
