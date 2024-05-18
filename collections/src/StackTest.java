f076f62f17c0fb62918621bdedd55bb9934ac9d3
DJL1n <djl1n_private@163.com>
2024-05-18 13:32:37 +0800

    2024.5.18

--- collections/src/StackTest.java
+++ collections/src/StackTest.java
@@ -1,5 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StackTest {
     public static void main(String[] args) {
-
+        Deque<String> stack = new ArrayDeque<>();
+        for (String s : "My dog has fleas".split(" ")) {
+            stack.push(s);
+        }
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop() + " ");
+        }
     }
 }
