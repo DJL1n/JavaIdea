df80cb13ef950369cc85b88df601a623504c2c96
DJL1n <djl1n_private@163.com>
2024-05-20 20:19:55 +0800

    2024.5.20

--- hdfs/src/main/java/com/root/sort/SortMapper.java
+++ hdfs/src/main/java/com/root/sort/SortMapper.java
@@ -10,15 +10,11 @@ import java.io.IOException;
 
 public class SortMapper extends Mapper<LongWritable, Text, IntWritable, NullWritable> {
     private IntWritable number = new IntWritable();
-    private int count = 0;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if (count < 20) { // 只处理前 20 个数字
-            int num = Integer.parseInt(value.toString());
-            number.set(num);
-            context.write(number, NullWritable.get()); // 输出键值对 (number, NullWritable)
-            count++; // 计数加一
-        }
+        int num = Integer.parseInt(value.toString());
+        number.set(num);
+        context.write(number, NullWritable.get()); // 输出键值对 (number, NullWritable)
     }
 }
