6481e83ca91b5ae27bfcc52b9ac423a55a822388
DJL1n <djl1n_private@163.com>
2024-05-20 19:41:22 +0800

    2024.5.20

--- hdfs/src/main/java/com/root/sort/SortMapper.java
+++ hdfs/src/main/java/com/root/sort/SortMapper.java
@@ -2,21 +2,19 @@ package com.root.sort;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class SortMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
-    Text k=new Text();
-    IntWritable v=new IntWritable();
-    protected void map(LongWritable key,Text value, Context context) throws IOException,InterruptedException{
-        String line=value.toString();
-        String[] words=line.split(" ");
-        for (String word : words) {
-            k.set(word);
-            v.set(1);
-            context.write(k,v);
-        }
+public class SortMapper extends Mapper<LongWritable, Text, IntWritable, NullWritable> {
+    private IntWritable number = new IntWritable();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        int num = Integer.parseInt(value.toString());
+        number.set(num);
+        context.write(number, NullWritable.get()); // 输出键值对 (number, NullWritable)
     }
 }
--- hdfs/src/main/java/com/root/sort/SortReducer.java
+++ hdfs/src/main/java/com/root/sort/SortReducer.java
@@ -1,19 +1,20 @@
 package com.root.sort;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class SortReducer extends Reducer<Text, IntWritable, Text, IntWritable>  {
-    IntWritable v=new IntWritable();
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
-        int sum=0;
-        for (IntWritable value : values) {
-            sum+=value.get();
+public class SortReducer extends Reducer<IntWritable, NullWritable, IntWritable, IntWritable> {
+    private IntWritable lineNumber = new IntWritable(1);
+
+    @Override
+    protected void reduce(IntWritable key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+        for (NullWritable value : values) {
+            context.write(lineNumber, key); // 输出序号和数值 (lineNumber, sorted number)
+            lineNumber.set(lineNumber.get() + 1); // 序号递增
         }
-        v.set(sum);
-        context.write(key,v);
     }
 }
+
