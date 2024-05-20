f0168e0de6a3d250aad11d658beade33425588e7
DJL1n <djl1n_private@163.com>
2024-05-20 19:42:26 +0800

    2024.5.20

--- hdfs/src/main/java/com/root/sort/SortDriver.java
+++ hdfs/src/main/java/com/root/sort/SortDriver.java
@@ -3,7 +3,7 @@ package com.root.sort;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -12,20 +12,21 @@ import java.io.IOException;
 
 public class SortDriver {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
-        Configuration conf=new Configuration();
-        Job job=Job.getInstance(conf);
-        job.setJarByClass(SortDriver.class);
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Sort Numbers");
 
+        job.setJarByClass(SortDriver.class);
         job.setMapperClass(SortMapper.class);
         job.setReducerClass(SortReducer.class);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(NullWritable.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
 
-        FileInputFormat.setInputPaths(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        boolean res=job.waitForCompletion(true);
-        System.out.println(res ? 0 : 1);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
