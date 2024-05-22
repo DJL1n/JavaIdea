6eedec6327a4abd4bf44ab5646e98dcb7e459d8b
DJL1n <djl1n_private@163.com>
2024-05-22 17:11:10 +0800

    2024.5.22

---
+++ functional/src/RecursiveFactorial.java
@@ -0,0 +1,14 @@
+interface IntCall{
+    int call(int arg);
+}
+
+public class RecursiveFactorial {
+    static IntCall fact;
+
+    public static void main(String[] args) {
+        fact=n->n==0?1:n*fact.call(n-1);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fact.call(i));
+        }
+    }
+}
