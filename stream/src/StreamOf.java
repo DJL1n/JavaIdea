4332a989793478698f98b5a75e95455120e4e496
DJL1n <djl1n_private@163.com>
2024-05-22 21:32:22 +0800

    2024.5.22

--- stream/src/StreamOf.java
+++ stream/src/StreamOf.java
@@ -1,3 +1,9 @@
-public class StreamOf {
+import java.util.stream.Stream;
 
+public class StreamOf {
+    public static void main(String[] args) {
+        Stream.of(
+                new Bubble(1)
+        )
+    }
 }
