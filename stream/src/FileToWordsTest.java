ee577cc19caf392e43c07ea1d22b455c59b5ee43
DJL1n <djl1n_private@163.com>
2024-05-23 21:15:51 +0800

    2024.5.23

--- stream/src/FileToWordsTest.java
+++ stream/src/FileToWordsTest.java
@@ -1,6 +1,12 @@
 public class FileToWordsTest {
     public static void main(String[] args) throws Exception{
-
-
+        FileToWords.stream("E:\\临时\\软件学习\\JavaIdea\\stream\\src\\FunctionMap.java")
+                .limit(7)
+                .forEach(s -> System.out.format("%s ",s));
+        System.out.println();
+        FileToWords.stream("E:\\临时\\软件学习\\JavaIdea\\stream\\src\\FunctionMap.java")
+                .skip(7)
+                .limit(7)
+                .forEach(s -> System.out.format("%s ",s));
     }
 }
