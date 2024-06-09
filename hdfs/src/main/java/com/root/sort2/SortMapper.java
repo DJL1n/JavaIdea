9a15ffeeafc6a7184ca57cfbe0be185318b050a2
DJL1n <djl1n_private@163.com>
2024-06-09 13:20:04 +0800

    2024.6.9

--- hdfs/src/main/java/com/root/sort2/SortMapper.java
+++ hdfs/src/main/java/com/root/sort2/SortMapper.java
@@ -2,19 +2,20 @@ package com.root.sort2;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class SortMapper extends Mapper<LongWritable, Text, IntWritable, NullWritable> {
-    private IntWritable number = new IntWritable();
+public class SortMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
+    private static final IntWritable number = new IntWritable();
+    private static final Text emptyText = new Text("");
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        int num = Integer.parseInt(value.toString());
+        int num = Integer.parseInt(value.toString().trim());
         number.set(num);
-        context.write(number, NullWritable.get());
+        context.write(number, emptyText);
     }
 }
+
--- hdfs/src/main/java/com/root/sort2/SortReducer.java
+++ hdfs/src/main/java/com/root/sort2/SortReducer.java
@@ -2,18 +2,31 @@ package com.root.sort2;
 
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class SortReducer extends Reducer<IntWritable, NullWritable, IntWritable, IntWritable> {
-    private IntWritable lineNumber = new IntWritable(1);
+public class SortReducer extends Reducer<IntWritable, Text, Text, IntWritable> {
+    private static int counter = 1;
+    private static final Text numberedText = new Text();
 
     @Override
-    protected void reduce(IntWritable key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-        context.write(lineNumber, key);
-        lineNumber.set(lineNumber.get() + 1);
+    protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        List<Integer> numbers = new ArrayList<>();
+        for (Text value : values) {
+            numbers.add(key.get());
+        }
+
+        Collections.sort(numbers);
+
+        for (Integer num : numbers) {
+            numberedText.set(counter + ".");
+            context.write(numberedText, new IntWritable(num));
+            counter++;
+        }
     }
 }
