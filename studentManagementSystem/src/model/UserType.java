e9ed34b089def9c98aeeb63dac76a7f2ec857c2e
DJL1n <djl1n_private@163.com>
2024-06-22 17:37:27 +0800

    6.22

--- studentManagementSystem/src/model/UserType.java
+++ studentManagementSystem/src/model/UserType.java
@@ -1,11 +1,22 @@
 package model;
-//枚举类，，用户类型
+
+/**
+ * 枚举类，用于定义系统中不同用户类型。
+ * 包含系统管理员、学生和教师三种用户类型，并为每种类型赋予了特定的索引值。
+ */
 public enum UserType {
-    ADMIN("系统管理员",0),
-    STUDENT("学生",1),
-    TEACHER("教师",2);
+    // 系统管理员，索引值为0
+    ADMIN("系统管理员", 0),
+    // 学生，索引值为1
+    STUDENT("学生", 1),
+    // 教师，索引值为2
+    TEACHER("教师", 2);
+
+    // 用户类型的名称
     private String name;
+    // 用户类型的索引值，用于在程序中标识不同的用户类型
     private int index;
+
     private UserType(String name,int index) {
         this.name=name;
         this.index=index;
