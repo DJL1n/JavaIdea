d9a617499ce61d7410c77ed0ae49903b5e8c29b9
DJL1n <djl1n_private@163.com>
2024-06-22 17:18:25 +0800

    6.22

--- studentManagementSystem/src/model/Teacher.java
+++ studentManagementSystem/src/model/Teacher.java
@@ -1,29 +1,39 @@
 package model;
-//教师类
+
+/**
+ * 教师类，用于表示教师管理系统中的一个教师。
+ * 包含教师的基本信息，如ID、姓名、密码和所教授的课程名称。
+ */
 public class Teacher {
+    // 教师的ID，唯一标识一个教师
     private int id;
+    // 教师的姓名
     private String name;
+    // 教师的密码，用于登录系统
     private String password;
+    // 教师所教授的课程名称
     private String TeachCourseName;
 
-    public Teacher(){
+    public Teacher(){}
 
-    }
     public Teacher(String name,String password){
         this.name = name;
         this.password = password;
     }
+
     public Teacher(int id,String name,String password){
         this.id = id;
         this.name = name;
         this.password = password;
     }
+
     public Teacher(int id,String name,String password,String TeachCourseName){
         this.id = id;
         this.name = name;
         this.password = password;
         this.TeachCourseName = TeachCourseName;
     }
+
     public int getId() {
         return id;
     }
