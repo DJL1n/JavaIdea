eeeefa5ad57cae1b850bb31c6b81f364e922bf17
DJL1n <djl1n_private@163.com>
2024-06-22 17:03:21 +0800

    6.22

--- studentManagementSystem/src/model/Student.java
+++ studentManagementSystem/src/model/Student.java
@@ -1,25 +1,36 @@
 package model;
 
+/**
+ * 学生类，用于表示学生管理系统中的一个学生。
+ * 包含学生的基本信息，如ID、姓名、密码、性别、年龄和系部。
+ */
 public class Student {
+    // 学生的ID，唯一标识一个学生
     private int id;
+    // 学生的姓名
     private String name;
+    // 学生的密码，用于登录系统
     private String password;
+    // 学生的性别
     private String sex;
+    // 学生的年龄
     private int age;
+    // 学生所属的系部或学院
     private String dept;
-//学生类
-    public Student(){
 
-    }
+    public Student(){}
+
     public Student(String name,String password){
         this.name = name;
         this.password = password;
     }
+
     public Student(int id,String name,String password){
         this.id = id;
         this.name = name;
         this.password = password;
     }
+
     public Student(int id,String name,String sex,int age,String dept) {
         this.id = id;
         this.name = name;
@@ -27,6 +38,7 @@ public class Student {
         this.age = age;
         this.dept = dept;
     }
+
     public int getId() {
         return id;
     }
