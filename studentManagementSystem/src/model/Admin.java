007e62059d3fd1cb6300b17f1ab8a305f2a9c700
DJL1n <djl1n_private@163.com>
2024-06-22 17:00:48 +0800

    6.22

--- studentManagementSystem/src/model/Admin.java
+++ studentManagementSystem/src/model/Admin.java
@@ -1,22 +1,30 @@
 package model;
-//管理员类
+
+/**
+ * 管理员类，用于表示系统中的管理员账户。
+ * 包含管理员的ID、姓名和密码等属性。
+ */
 public class Admin {
+    // 管理员的ID
     private int id;
+    // 管理员的姓名
     private String name;
+    // 管理员的密码
     private String password;
 
-    public Admin(){
+    public Admin(){}
 
-    }
     public Admin(String name,String password){
         this.name = name;
         this.password = password;
     }
+
     public Admin(int id,String name,String password){
         this.id = id;
         this.name = name;
         this.password = password;
     }
+
     public int getId() {
         return id;
     }
