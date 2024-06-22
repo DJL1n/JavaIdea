d6e952f2f4ed73a1a834423c8b540ba679f29a5a
DJL1n <djl1n_private@163.com>
2024-06-22 15:43:55 +0800

    6.22

--- studentManagementSystem/src/dao/BaseDao.java
+++ studentManagementSystem/src/dao/BaseDao.java
@@ -1,16 +1,24 @@
 package dao;
 
 import util.DBUtil;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-//Base类，连接到数据库
+
+/**
+ * 数据访问对象的基类，提供数据库连接和关闭连接的方法。
+ */
 public abstract class BaseDao {
+    // 数据库连接对象
     protected Connection con = DBUtil.getConnection();
+    // 预编译SQL语句对象
     protected PreparedStatement pStatement = null;
+
+    /**
+     * 关闭数据库连接的方法。
+     */
     protected void close(){
-        try{
+        try {
             this.con.close();
         } catch (SQLException e) {
             e.printStackTrace();
